4c2d2d87b7f441b7cc5e169076471be956a6e2b8
Brandon Bremen <bbremen@gmail.com>
2013-08-04 16:56:03 -0700

    handle multiple lines of input
    instead of disconnecting after handling one line,
    accept the next line of input after handling the first.
    
    turned off input and output buffering
    colorize output
    added exit command
    added stub walk handler
    made a session-ctx struct that handlers receive

--- handlers.rkt
+++ handlers.rkt
@@ -2,18 +2,40 @@
 
 ; expose function to find a handler
 
-(provide get-handler)
+(provide get-handler session-ctx)
+
+; session information passed to handlers
+(struct session-ctx (command args print))
 
 (define dispatch-table (make-hash))
 
 (define (get-handler name)
   (hash-ref dispatch-table name #f))
 
+; a handler optionally returns a function
+; that handles the next line of input
 (define (create-handler name fn)
   (hash-set! dispatch-table name fn))
 
+; prints handler output and returns false
+; indicating that the handler is done
+(define (single-line-handler name fn)
+  (create-handler name fn)
+  #f)
+
 ; command handlers
 
-(create-handler "hello"
-           (lambda (args)
-             "Hello, World!"))
+(single-line-handler "hello"
+           (lambda (session)
+             ((session-ctx-print session) "Hello, World!")))
+
+(single-line-handler "walk"
+           (lambda (session)
+             (define args (session-ctx-args session))
+             (define print (session-ctx-print session))
+
+             (if (empty? args)
+                 (print "where do you want to walk?")
+                 (print (string-join
+                         (list "walking" (first (session-ctx-args session))))))))
+
--- server.rkt
+++ server.rkt
@@ -22,27 +22,78 @@
   (define cust (make-custodian))
   (custodian-limit-memory cust (* 10 1024 1024))
   (parameterize ([current-custodian cust])
-    (define-values (in out) (tcp-accept listener))    
+    (define-values (in out) (tcp-accept listener))
+    (file-stream-buffer-mode out 'none)
+    (file-stream-buffer-mode in 'none)
     (thread
      (lambda()
+       (print-banner out)
        (handle in out)
        (close-input-port in)
        (close-output-port out)))))
 
+(define (empty-trimmed-string? str)
+      (zero? (string-length (string-trim str))))
+
+(define (color-value color-name)
+  (number->string
+   (case color-name
+     [('black) 30]
+     ['red 31]
+     ['green 32]
+     ['yellow 33]
+     ['blue 34]
+     ['magenta 35]
+     ['cyan 36]
+     ['white 37])))
+
+(define (colorize color text)
+  (string-append (bytes->string/utf-8 #"\033\133") (color-value color) "m"
+                 text
+                 (bytes->string/utf-8 #"\033\1330m")))
+
+(define (print-banner out)
+  (display (colorize 'yellow "=== ") out)
+  (display (colorize 'cyan "WELCOME") out)
+  (displayln (colorize 'yellow " ===") out))
+
+(define (print-prompt out)
+  (display (colorize 'red ">") out)
+  (display " " out))
+
 (define (handle in out)
-    ; extract command and arguments
-  (define words (string-split (read-line in)))
-  (define command (first words))
-  (define args (rest words))
-
-  (displayln (string-join words))
+  ; extract command and arguments
   
-  (displayln (dispatch command args) out))
+  (print-prompt out)
+  (define line (read-line in))
+  (displayln line)
+  
+  (cond
+    [(eof-object? line) #f]
+    [(equal? (string-trim line) "exit")
+     (displayln "goodbye" out)]
+    [(empty-trimmed-string? line) (handle in out)]
+    [else
+     (if current-handler
+         (current-handler line)
+         (next-command line out)
+         )
+     (handle in out)]))
 
-(define (dispatch command args)
+(define current-handler #f)
+
+(define (next-command line out)
+  (define words (string-split line))
+  (define command (first words))
+  (define (print text) (displayln text out))
+  (define session (session-ctx command
+                               (rest words)
+                               print))
   (define handler (get-handler command))
-
+  
+  ; TODO return current-handler from handler
   (if handler
+      (handler session)
       (begin
-        (handler args))
-      '"I don't understand"))
+        (print "I don't understand")
+        #f)))
